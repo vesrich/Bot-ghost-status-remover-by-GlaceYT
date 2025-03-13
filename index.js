baa747d916a514b5fceee618221ef871a38e75f9
vesrich <147327401+vesrich@users.noreply.github.com>
2025-03-13 21:07:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function login() {
 }
 
 function updateStatus() {
-  const currentStatus = ["/luck"];
+  const currentStatus = statusMessages[currentStatusIndex];
   const currentType = statusTypes[currentTypeIndex];
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom }],
